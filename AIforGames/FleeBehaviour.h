29e8a6658b84cf0618f3c47dfac81a36d18fc775
Bonface Njuguna <bonface.njuguna@learninglions.org>
2022-05-26 17:19:49 +0300

    added wander and flee behaviour

---
+++ AIforGames/FleeBehaviour.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Behaviour.h"
+#include <raymath.h>
+#include <vector>
+
+class FleeBehaviour :
+	public Behaviour
+{
+public:
+	FleeBehaviour() {};
+	virtual ~FleeBehaviour() {};
+
+	virtual bool Update(Agent* agent, float deltaTime);
+	void SetDestination(Vector2 destination) { m_destination = destination; }
+
+private:
+	Vector2 m_destination;
+	float m_fleeRadius = 100;
+};
